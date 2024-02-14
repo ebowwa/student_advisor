97a6f4f41f06e223d2d43a53b32deb5108e4708c
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-02-14 09:07:42 +0000

    update

---
+++ app/privacy/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrivacyPolicyPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy content...</p>
+    </div>
+  );
+}
+
+export default PrivacyPolicyPage;
