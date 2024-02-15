b4820aa8a4fe292b98d651ed407c1f7706bf43d7
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-02-15 18:00:30 +0000

    see

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 // app/home/page.tsx
 "use client";
 
-import { HomePage } from "../../components/HomePage"; // Adjust the import path based on your project structure
+import { HomePage } from "@/components/HomePage"; // Adjust the import path based on your project structure
 
 export default function Home() {
   return <HomePage />;
